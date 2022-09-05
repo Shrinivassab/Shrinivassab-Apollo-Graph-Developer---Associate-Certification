ab2fa2996c03ada9f95a62e18ed0fe803f9ec910
shrinivassab <shrinivassab@gmail.com>
2022-09-05 09:32:46 -0400

    Lift-off III: Arguments

--- client/src/containers/track-card.js
+++ client/src/containers/track-card.js
@@ -2,42 +2,42 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { colors, mq } from '../styles';
 import { humanReadableTimeFromSeconds } from '../utils/helpers';
+import { Link } from '@reach/router';
 
 /**
  * Track Card component renders basic info in a card format
  * for each track populating the tracks grid homepage.
  */
 const TrackCard = ({ track }) => {
-  const { title, thumbnail, author, length, modulesCount } = track;
+  const { title, thumbnail, author, length, modulesCount, id } = track;
 
   return (
-    <CardContainer>
-      <CardContent>
-        <CardImageContainer>
-          <CardImage src={thumbnail} alt={title} />
-        </CardImageContainer>
-        <CardBody>
-          <CardTitle>{title || ''}</CardTitle>
-          <CardFooter>
-            <AuthorImage src={author.photo} />
-            <AuthorAndTrack>
-              <AuthorName>{author.name}</AuthorName>
-              <TrackLength>
-                {modulesCount} modules -{' '}
-                {humanReadableTimeFromSeconds(length)}
-              </TrackLength>
-            </AuthorAndTrack>
-          </CardFooter>
-        </CardBody>
-      </CardContent>
-    </CardContainer>
-  );
+      <CardContainer to={`/track/${id}`}>
+<CardContent>
+  <CardImageContainer>
+  <CardImage src={thumbnail} alt={title} />
+  </CardImageContainer>
+  <CardBody>
+  <CardTitle>{title || ''}</CardTitle>
+  <CardFooter>
+  <AuthorImage src={author.photo} />
+  <AuthorAndTrack>
+  <AuthorName>{author.name}</AuthorName>
+  <TrackLength>
+  {modulesCount} modules - {humanReadableTimeFromSeconds(length)}
+  </TrackLength>
+  </AuthorAndTrack>
+  </CardFooter>
+  </CardBody>
+  </CardContent>
+  </CardContainer>
+);
 };
 
 export default TrackCard;
 
 /** Track Card styled components */
-const CardContainer = styled.div({
+const CardContainer = styled(Link)({
   borderRadius: 6,
   color: colors.text,
   backgroundSize: 'cover',
@@ -64,6 +64,7 @@ const CardContainer = styled.div({
     backgroundColor: colors.pink.lightest,
   },
   cursor: 'pointer',
+  textDecoration: 'none',
 });
 
 const CardContent = styled.div({
